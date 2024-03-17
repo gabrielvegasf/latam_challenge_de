bf5c08a11128c731822b2c695a95565f1caae814
gabrielvegasf <gvegas@gmail.com>
2024-03-17 20:01:26 -0300

    Ajustes de q1

--- src/config.py
+++ src/config.py
@@ -18,8 +18,8 @@ data_path = "../data/"
 # ---------------------
 
 
-data_file = "test_01.json"
-# data_file = "farmers-protest-tweets-2021-2-4.json"
+#data_file = "test_01.json"
+data_file = "farmers-protest-tweets-2021-2-4.json"
 # ---------------------
 # -- Nombre de archivo de datos 
 # ---------------------
