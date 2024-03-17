0fdae4c4e5ac4b60bbb289be7439377ff2162745
gabrielvegasf <gvegas@gmail.com>
2024-03-17 13:37:29 -0300

    Optimizancion Spark q1 time

--- src/config.py
+++ src/config.py
@@ -1,6 +1,81 @@
+# ---------------------
+# -- config.py
+# -- proyecto: latam_challenge_de
+# -- Archivo de configuracion del proyecto
+# -- marzo 2024
+# ---------------------
+project_name = "latam_challenge_de"
+
 
 data_path = "../data/"
+# ---------------------
+# -- Ruta abosluta, relatva, o en cloud del Repositorio de datos
+# -- Ej: ../repsitorio/ 
+# -- 		/ruta/del/repsitorio/
+# -- 		gs://usuario-google-bucket/
+# -- 		wasbs://azure-container@<azure-storage-account>.blob.core.windows.net/
+# -- 		
+# ---------------------
+
+
 data_file = "test_01.json"
 # data_file = "farmers-protest-tweets-2021-2-4.json"
+# ---------------------
+# -- Nombre de archivo de datos 
+# ---------------------
+
+
+
+data_read_size = 100000
+# ---------------------
+# -- Indica Cantidad de registros a leer para realizar ejecucion por hilos (Threads).
+# ---------------------
+
+
+
+
+use_spark_service = False
+# ---------------------
+# -- Indica si usa un servicio de Apache Spark.
+# -- Si es False , la carga de archivos se realiza directa por filesystem
+# ---------------------
+
+
+spark_service_provider = "local"
+# ---------------------
+# -- Proveedor del Servicio Apche Spark. Puede ser un servicio local o en cloud
+# -- Dependiendo del servicio se configura parametros particulares de cada proveedor en su seccion.
+# -- Si la variable <use_spark_service> == False, se realiza una carga directa por filesystem y se asume el valor de <spark_service_provider> como "local"
+# -- 
+# -- Proveedores:
+# -- local:  Servicio SAPRK local en el mismo servidor o mismo segmento de red
+# -- gcp: Servicio Google Cloud Platform , Google Cloud Storage (GCS), BigQuery.
+# -- azure: Servicio Azure HDInsight.
+# ---------------------
+
+
+# ---------------------
+# Configuracion Local
+# ---------------------
+
+SPARK_IP = "127.0.0.1"
+
+
+# ---------------------
+# Configuracion GCP
+# ---------------------
+
+SPARK_GCP_JARS = "gs://ruta/a/archivos/jar_files/*.jar"
+SPARK_GCP_SERV_ACCOUNT_FILE = "/ruta/a/archivo/service-account-file.json"
+
+
+# ---------------------
+# Configuracion Azure
+# ---------------------
+
+SPARK_AZURE_STORAGE_ACCOUNT = "cuenta_azure"
+SPARK_AZURE_STORAGE_ACCOUNT_KEY = "cuenta_azure_key"
+
+
 
 
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,13 +1,11 @@
 import os
 import importlib
-import json
 import concurrent.futures
-import pandas as pd
-import pyspark
 from multiprocessing import Pool
 from pyspark.sql import SparkSession
 from collections import defaultdict
 from datetime import datetime
+import json
 
 import resource
 import humanize
@@ -16,94 +14,123 @@ from typing import List, Tuple
 
 #@profile
 
-def procesar_data_file(data_bloque, tweets_fecha, tweets_fecha_user):
-
-    data = data_bloque.collect()
-
-    for row in data:
-
-        tweet = json.loads(row['value'])
-
-        fecha_datetime = tweet['date']
-        fecha = fecha_datetime[0:10]
-
-        print((fecha))
-
-        username = tweet['user']['username']
-
-        print((username))
-        
-        # Incrementa el contador para la fecha correspondiente
-        tweets_fecha[fecha] += 1
-        tweets_fecha_user[fecha][username] += 1
-
-
-def read_dataframe(sdf, registro_actual, data_size=10000):
-
-    while True:
-
-        if (registro_actual < sdf.count()):
-
-            df_bloque = sdf.offset(registro_actual).limit(data_size)
-            registro_actual += data_size
-
-            yield df_bloque
-        else:
-            break
 
 
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
 
-    data_size = 20  # Registro por bloque
+
+    def procesar_data_file(data_bloque, tweets_fecha, tweets_fecha_user):
+
+        # ---------------------
+        # Recibe un bloque de datos y procesa cada registro preocesa convirtiendo el JSON
+        # 
+        # Param:
+        #   data_bloque: Lista con los data del bloque. Esta lista puede ser de varios tipos: Row de Spark Data Frame o un Objeto File Object
+        #   tweets_fecha: defaultdict que alamcena en conteo de tweets por fecha
+        #   tweets_fecha_user: defaultdict que alamcena en conteo de tweets por fecha y usuario
+        # 
+        # Retorna:
+        #   
+        # ---------------------
+
+        
+        for row in data_bloque:
+
+
+            if isinstance(row, str):
+                tweet = json.loads(row)
+            else:
+                tweet = json.loads(row['value'])
+
+            fecha_datetime = tweet['date']
+            fecha = fecha_datetime[0:10]
+
+            username = tweet['user']['username']
+
+            # Incrementa el contador para la fecha correspondiente
+            tweets_fecha[fecha] += 1
+            tweets_fecha_user[fecha][username] += 1
+
+    # ---------------------
+
+
     tweets_fecha = defaultdict(int)
     tweets_fecha_user = defaultdict(lambda: defaultdict(int))
     lista_resultado_tuplas = []
 
-    # Crear una sesión de Spark
-    objSpark = SparkSession.builder \
-        .appName("q1_time_using_spark") \
-        .config("spark.logLevel", "WARN") \
-        .getOrCreate()
-
-    # Leer el archivo a un Spark DataFrame 
-    sdf = objSpark.read.text(file_path)
-
-    total_registros = sdf.count()
-    registro_actual = 0
+    config = importlib.import_module("config")
+    common_functions = importlib.import_module("common_functions")
 
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        
-        # Procesar cada segmento del archivo en un hilo separado
-        futures = [executor.submit(procesar_data_file, data_bloque, tweets_fecha, tweets_fecha_user) for data_bloque in read_dataframe(sdf, registro_actual, data_size)]
+    if file_path == "":
+        # Si el archivo no es especificado se usa los indicados en Config
+        file_path = config.data_path + config.data_file
 
-        # Esperar a que todos los hilos terminen
-        for future in concurrent.futures.as_completed(futures):
-            try:
-                future.result()  # Obtener el resultado del hilo (si es necesario)
-            except Exception as e:
-                print(f'Ocurrió un error: {e}')
+    bValid, sMsg = common_functions.validate_file(file_path)
+
+    if not bValid:
+        print(sMsg)
+        exit(0)
 
 
+    if config.use_spark_service == True:
+        # ---------------------
+        # Se procesa la data usando el servico Spark Especificado y se realiza con Threads
+        # ---------------------
 
-    # Detener la sesión de Spark
-    objSpark.stop()
+        # Creamos una Session Spark
+        objSpark = common_functions.create_spark_session()
 
-#    exit(0)
+        # Leer el archivo a un Spark DataFrame 
+        sdf = objSpark.read.text(file_path)
+
+        registro_actual = 0
+
+        data_size = config.data_read_size  # Registro por bloque 
+
+        # Usamos una estrategia de lectura en bloques para hacer proceamiento en Threads
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            
+            # Procesar cada bloque del archivo en un hilo separado
+            futures = [executor.submit(procesar_data_file, data_bloque, tweets_fecha, tweets_fecha_user) for data_bloque in common_functions.read_spark_dataframe(sdf, registro_actual, data_size)]
+
+            # Esperar a que todos los hilos terminen
+            for future in concurrent.futures.as_completed(futures):
+                try:
+                    future.result()  
+                except Exception as e:
+                    print(f'Ocurrió un error: {e}')
+
+        # Detener la sesión de Spark
+        objSpark.stop()
+
+    else :
+        # ---------------------
+        # Se procesa la data Realizando una lectura del archivo via filesystem
+        # ---------------------
+
+        # Abre el archivo JSON en modo lectura
+        with open(file_path, 'r') as archivo:
+
+            procesar_data_file(archivo, tweets_fecha, tweets_fecha_user)
+
+
+    # De cualquier metodo alicado se tiene la data compilada en las estructuras "tweets_fecha" y "tweets_fecha_user"
+    # Ordenamos las lista tweets_fecha de forma descendente por su valor y lo restringimos al TOP N
 
     top_10_tweets_fecha_ordenado = sorted(tweets_fecha.items(), key=lambda item: item[1], reverse=True)[:10]
     print(top_10_tweets_fecha_ordenado)
 
-    # Imprime el total de tweets por fecha
+    # Del Top N obtenido buscamos en tweets_fecha_user para obtner los users que tienen mas tweets esa fecha
+    
     for fecha, total_tweets in top_10_tweets_fecha_ordenado:
-        # print(f"Fecha: {fecha}, Total de tweets: {total_tweets}")
 
-        # print("-----------")
-
+        # Ordenamos las lista tweets_fecha_user de forma descendente por su valor y lo restringimos al TOP N
         top_1_tweets_fecha_user = sorted(tweets_fecha_user[fecha].items(), key=lambda item: item[1], reverse=True)[:1]
 
         top_user_fecha, valor = top_1_tweets_fecha_user[0]
 
+        #Formateamos la fecha en el especificado
         fecha_date = datetime.strptime(fecha, "%Y-%m-%d").date()
 
         lista_resultado_tuplas.append((fecha_date, top_user_fecha))
@@ -114,24 +141,10 @@ def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
 
 if __name__ == "__main__":
 
-    config = importlib.import_module("config")
-
-    if not os.path.isfile(config.data_path + config.data_file):
-        print(f"Archivo de datos: {config.data_file} no existe")
-        exit(0)
-    else :
-        if os.stat(config.data_path + config.data_file).st_size == 0:
-            print(f"Archivo de datos: {config.data_file} vacio")
-            exit(0)
-
-    os.environ["SPARK_LOCAL_IP"] = "127.0.0.1"
-
-
-    resultado = q1_time(config.data_path + config.data_file)
+    resultado = q1_time("")
 
     print(resultado)
 
-    print('File Size ', humanize.naturalsize(os.stat(config.data_path + config.data_file).st_size))
     print('Peak Memory Usage =', humanize.naturalsize(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
     print('User Mode Time =', resource.getrusage(resource.RUSAGE_SELF).ru_utime)
     print('System Mode Time =', resource.getrusage(resource.RUSAGE_SELF).ru_stime)
